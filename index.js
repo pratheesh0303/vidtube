4afc45976041f5181f3acb25e3e4370cff7faba7
Pratheesh Lakshmanan <pratheeshl@hifx.co.in>
2022-11-08 18:06:30 +0530

    push

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 App.use(express.static('client/build'));
 App.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname + 'client/build/index.html'));
     });
 dotenv.config();
 const connect = ()=>{
